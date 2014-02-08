0dd427b9e395c0b3db5c5c64ac7671a805c77f97
Simon Leblanc <contact@leblanc-simon.eu>
2014-02-08 14:06:40 +0100

    Add alert if there are errors

--- web/js/main.js
+++ web/js/main.js
@@ -16,19 +16,26 @@ $(document).ready(function(){
             dataType: 'html',
             statusCode: {
                 200: function(data) {
+                    $(this).find('#alert').addClass('hide');
+                    $(this).find('#alert-content').html('');
                     $(this).find('#markdown > pre').html(data);
                 },
                 204: function() {
+                    $(this).find('#alert').addClass('hide');
+                    $(this).find('#alert-content').html('');
                     $(this).find('#markdown > pre').html('');
                 },
                 400: function() {
-                    
+                    $(this).find('#alert').removeClass('hide');
+                    $(this).find('#alert-content').html('Bad request : no content send in the server');
                 },
                 405: function() {
-
+                    $(this).find('#alert').removeClass('hide');
+                    $(this).find('#alert-content').html('Bad request : content no send with POST method');
                 },
                 500: function() {
-
+                    $(this).find('#alert').removeClass('hide');
+                    $(this).find('#alert-content').html('Big error : contact administrator');
                 }
             }
         });

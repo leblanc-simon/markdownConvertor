3df55e86c0895fc362032071fef7471be7c04a7f
Simon Leblanc <contact@leblanc-simon.eu>
2014-02-08 13:11:46 +0100

    First design of the page

---
+++ web/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="http://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic|Source+Code+Pro" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/main.css">
+        <title>markdownConvertor</title>
+    </head>
+    <body>
+        <div id="content">
+            <h1>markdownConvertor</h1>
+
+            <div id="source">
+                <h2>Source</h2>
+                <textarea></textarea>
+            </div>
+            <div id="markdown">
+                <h2>Result</h2>
+                <pre></pre>
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
